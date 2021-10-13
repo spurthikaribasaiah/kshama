50b111e100e85e332763741f4a54d0250e084464
spurthikaribasaiah <73448701+spurthikaribasaiah@users.noreply.github.com>
2021-10-13 11:25:55 +1100

    Update searchengine.py

--- searchengine.py
+++ searchengine.py
@@ -1,8 +1,16 @@
 import os, string
+from colorama import init
+from colorama import Fore as F
+from colorama import Back as B
+from colorama import Style
+
+
+def color(text, fore='', back=''):
+    return f'{fore}{back}{text}{Style.RESET_ALL}'
 
 class drivesearch:
     def list_available_drives(self):
-       available_drives = ['%s:' % d for d in string.ascii_lowercase if os.path.exists('%s:' % d)]
+       available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
        return available_drives
 
     def search_file_in_drives(self, filename, directorylist = None):
@@ -12,12 +20,10 @@ class drivesearch:
             dirlist = self.list_available_drives()
         else:
             dirlist = directorylist.split(',')
-        print(dirlist)
 
         # search for the file in the directories
         for dirname in dirlist:
-            dirname = dirname if ':' in dirname else dirname + ':'
-            print(dirname)
+            dirname = dirname.upper() if ':' in dirname else dirname.upper() + ':'
             for dirpath, dirs, files in os.walk(dirname):
                 for file in files:
                     if filename in file:
@@ -26,19 +32,26 @@ class drivesearch:
         return filelist
 
 if __name__ == "__main__":
-    print("welcome to the file search engine")
+
+    print(color("Welcome to the file search engine", F.BLUE))
 
     # Initialisation of the class
     drv_srch = drivesearch()
 
+    # Taking the user input to select the directory to search for the file and the specific file name to search
+    print(color("File Name is mandatory to put. Please input the file name to search for : ", F.RED))
+    file_name = input()
     # calling the function to list the available drives on the computer
     avail_drivs = drv_srch.list_available_drives()
-    print(avail_drivs)
-
-    # Taking the user input to select the directory to search for the file and the specific file name to search
-    file_name = input("File Name is mandatory to put. Please input the file name to search for : ")
-    directory_name = input("please select the directories to search from the above list mentioned eg:('c:','c:,h:') : ")
+    print(color("Please find the list of available directories:", F.RED))
+    print(color(avail_drivs, F.GREEN))
+    print(color("please select the directories to search from the above list mentioned eg:('C','C,H') : ", F.RED))
+    directory_name = input()
 
     # Search for the mentioned file in the directory.
     filelst = drv_srch.search_file_in_drives(file_name, directory_name)
-    print(filelst)
+    if not filelst:
+        print(color("There are no files present with the name '" + file_name + "' in the directory " + directory_name, F.RED))
+    else:
+        print(color("Please find below the list of search results for the file '" + file_name + "'", F.RED))
+        print(color(filelst, F.GREEN))
