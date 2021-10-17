f8d348ef92eb2030276fbe87dc56c81dcf106211
spurthikaribasaiah <73448701+spurthikaribasaiah@users.noreply.github.com>
2021-10-17 15:14:50 +1100

    Update listdirectories.py

--- searchengine/listdirectories.py
+++ searchengine/listdirectories.py
@@ -1,10 +1,14 @@
 import os, string
 
 
-def list_available_drives():
-    available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
-    return available_drives
+class listdrives:
+   def list_available_drives():
+       available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+       return available_drives
 
 if __name__ == '__main__':
-    avail_drivs = list_available_drives()
+#initialisation of class
+    list_drives = listdrives
+# calling the function with in class
+    avail_drivs = list_drives.list_available_drives()
     print(avail_drivs)
